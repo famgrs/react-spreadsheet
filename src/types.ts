63f43fc3b58d31b574a73d4cac0e00da6a738b93
famgrs <famgrs@gmail.com>
2022-01-19 23:06:06 -0300

    fix types.ts

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@ import { Point } from "./point";
 import { PointMap } from "./point-map";
 import { PointSet } from "./point-set";
 import { Matrix } from "./matrix";
-import { Selection } from "./selection";
+import { Selections } from "./selections";
 
 /** The base type of cell data in Spreadsheet */
 export type CellBase<Value = any> = {
@@ -46,7 +46,7 @@ export type Dimensions = {
 
 export type StoreState<Cell extends CellBase = CellBase> = {
   data: Matrix<Cell>;
-  selected: Selection;
+  selected: Selections;
   copied: PointMap<Cell>;
   hasPasted: boolean;
   cut: boolean;
